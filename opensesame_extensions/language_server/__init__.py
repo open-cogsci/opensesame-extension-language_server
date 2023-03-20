6b68de04edea74e1a782538f87ee62a5063c0290
smathot <s.mathot@cogsci.nl>
2023-03-20 08:49:32 +0100

    Fix dependencies for debs and wheels - Bump to 0.2.2

--- opensesame_extensions/language_server/__init__.py
+++ opensesame_extensions/language_server/__init__.py
@@ -1,5 +1,4 @@
 # The name of the package to check for updates on conda and pip
-packages = ['opensesame-extension-language_server',
-            'opensesame-extension-language-server',
-            'pyqode-language_server',
-            'pyqode3-language_server']
+packages = ['opensesame-extension-language-server',
+            'pyqode-language-server',
+            'pyqode3-language-server']
